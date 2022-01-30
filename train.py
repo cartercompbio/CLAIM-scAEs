59d3806c66b3092c6f18ac81c4ae16830cf0cf13
Clarence Mah <clarence.iyan@gmail.com>
2022-01-29 19:43:30 -0800

    add plotting frequency param, comment code

--- train.py
+++ train.py
@@ -7,18 +7,26 @@ def train_autoencoder(model,
                       criterion=torch.nn.MSELoss(reduction='sum'), 
                       optimizer=None, 
                       device="cpu", 
-                      num_epoch=50):
+                      num_epoch=50,
+                      plot_frequency=10):
     
     if optimizer == None:
         print("Using Adam optimizer with default learning rate")
         torch.optim.Adam(model.parameters())
         
+    # Variables to save training history
     liveloss = PlotLosses()
     loss_history = {}
+    
+    # Set model to training mode
     model.train()
+    
+    # Training loop for num_epochs
     for epoch in range(num_epoch+1):
         logs = {}
         running_loss = 0.0
+        
+        # Iterate through each batch
         for inputs in dataloader:
             inputs = inputs[0].to(device)
             outputs = model(inputs.float())
@@ -29,10 +37,15 @@ def train_autoencoder(model,
                 optimizer.step()            
             running_loss += loss.item()
 
+        # Save loss scaled for single sample
         epoch_loss = running_loss / len(dataloader.dataset)
         logs['loss'] = epoch_loss   
         loss_history.setdefault("train", []).append(epoch_loss)
     
+        # Update loss graph
         liveloss.update(logs)
-        liveloss.send()
+        
+        if epoch % plot_frequency == 0:
+            liveloss.send()
+        
     return loss_history, liveloss
